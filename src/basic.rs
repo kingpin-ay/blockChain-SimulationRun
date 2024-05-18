0c5bafa8d36d09957fd9b1687e55689630d5d22a
kingpin-ay <ayushmondal001@gmail.com>
2024-05-19 00:39:32 +0530

    refactor: code refactoration
    Refactored the code to use the same if else statement

--- src/basic.rs
+++ src/basic.rs
@@ -1,5 +1,6 @@
 use rand::rngs::OsRng;
 use rsa::pkcs1::{DecodeRsaPublicKey, EncodeRsaPrivateKey, EncodeRsaPublicKey};
+use rsa::pkcs8::DecodePrivateKey;
 use rsa::Pkcs1v15Encrypt;
 use rsa::{RsaPrivateKey, RsaPublicKey};
 use std::error::Error;
@@ -41,15 +42,20 @@ pub fn testing(file_path: &str) {
     // }
 }
 
+fn read_from_file(file_path: &str) -> Vec<u8> {
+    let mut file = File::open(file_path).unwrap();
+    let mut buffer = Vec::new();
+    let _ = file.read_to_end(&mut buffer);
+    return buffer;
+}
+
 pub fn encrypt_file_with_public_key(
     file_path: &str,
     public_key_path: &str,
     output_path: &str,
 ) -> Result<(), Box<dyn Error>> {
     // Open and read the file contents into a buffer
-    let mut file = File::open(file_path).unwrap();
-    let mut buffer = Vec::new();
-    let _ = file.read_to_end(&mut buffer);
+    let buffer = read_from_file(file_path);
 
     // Read the public key from the PEM file
     let public_key_pem: String = match std::fs::read_to_string(public_key_path) {
@@ -73,6 +79,36 @@ pub fn encrypt_file_with_public_key(
     // Write the encrypted data to the output file
     let mut output_file = File::create(output_path)?;
     output_file.write_all(&encrypted_data)?;
+
+    Ok(())
+}
+
+fn decrypt_file_with_private_key(
+    encrypted_file_path: &str,
+    private_key_path: &str,
+    output_path: &str,
+) -> Result<(), Box<dyn Error>> {
+    // Read the private key from the PEM file
+    let private_key_pem = match std::fs::read_to_string(private_key_path) {
+        Ok(contents) => contents,
+        Err(err) => return Err(Box::new(err)),
+    };
+    let private_key = match RsaPrivateKey::from_pkcs8_pem(&private_key_pem) {
+        Ok(contents) => contents,
+        Err(err) => return Err(Box::new(err)),
+    };
+
+    // Open and read the encrypted file contents into a buffer
+    let mut encrypted_file = File::open(encrypted_file_path)?;
+    let mut encrypted_buffer = Vec::new();
+    encrypted_file.read_to_end(&mut encrypted_buffer)?;
+
+    // Decrypt the buffer using the private key
+    let decrypted_data = private_key.decrypt(Pkcs1v15Encrypt, &encrypted_buffer)?;
+
+    // Write the decrypted data to the output file
+    let mut output_file = File::create(output_path)?;
+    output_file.write_all(&decrypted_data)?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,112 @@
 mod basic;
+use std::io;
+
+enum Cryptography {
+    ENCRYPTION,
+    DECRYPTION,
+}
 
 fn main() {
-    let private_key_path = "key/private_key.pem";
-    let public_key_path = "key/public_key.pem";
-    let file_path = "assets/example.txt";
-    println!("Generating the private and public key -> ");
+    const PRIVATE_KEY_PATH: &str = "key/private_key.pem";
+    const PUBLIC_KEY_PATH: &str = "key/public_key.pem";
+
+    loop {
+        println!("Choose Functionality : ");
+        println!("1. Generate Private key and Public key ");
+        println!("2. Encrypt a File  ");
+        println!("3. Decrypt a File  ");
+        println!("4. Exit   ");
+        let mut user_input = String::new();
+        let _ = match io::stdin().read_line(&mut user_input) {
+            Ok(content) => content,
+            Err(error) => {
+                println!("this is the error -> {}", error);
+                return ();
+            }
+        };
+
+        let user_choice_number: i32 = match user_input.trim().parse() {
+            Ok(content) => content,
+            Err(_err) => {
+                println!("Parsing Failed , Please Enter a Valid Number.");
+                return ();
+            }
+        };
+
+        if user_choice_number == 1 {
+            generate_key(PRIVATE_KEY_PATH, PUBLIC_KEY_PATH);
+        } else if user_choice_number == 2 {
+            encrypt_file(PUBLIC_KEY_PATH);
+        } else if user_choice_number == 3 {
+            decrypt_file(PRIVATE_KEY_PATH);
+        } else if user_choice_number == 4 {
+            break;
+        } else {
+            println!("Please Select A valid Option");
+        }
+    }
+}
+
+fn generate_key(private_key_path: &str, public_key_path: &str) {
     let _key_value = match basic::generate_rsa_key_pair(private_key_path, public_key_path) {
-        Ok(value) => value,
-        Err(_error) => (),
+        Ok(_value) => {
+            println!("Public key and private key Generation Complete");
+        }
+        Err(_error) => {
+            println!("Public key and private key Generation Failed");
+        }
     };
-    basic::testing(file_path);
+}
+
+fn encrypt_file(public_key_path: &str) {
+    let (file_path, encrypted_path) = take_file_input(Cryptography::ENCRYPTION);
+
     let _some_variable =
-        basic::encrypt_file_with_public_key(file_path, "key/public_key.pem", "assets/test.bin");
+        basic::encrypt_file_with_public_key(&file_path, public_key_path, &encrypted_path);
+}
+
+fn decrypt_file(private_key_path: &str) {
+    let (file_path, decrypted_path) = take_file_input(Cryptography::DECRYPTION);
+}
+
+fn take_file_input(action_type: Cryptography) -> (String, String) {
+    let mut file_path = String::new();
+    let mut encrypted_path = String::new();
+    let mut decrypted_path = String::new();
+
+    println!("Enter the file path: ");
+    let _ = match io::stdin().read_line(&mut file_path) {
+        Ok(_contents) => {}
+        Err(error) => {
+            println!("Was not able to take the file path input -> {}", error);
+        }
+    };
+    match action_type {
+        Cryptography::ENCRYPTION => {
+            println!("Enter the encrypted file path: ");
+            let _ = match io::stdin().read_line(&mut encrypted_path) {
+                Ok(_contents) => {}
+                Err(error) => {
+                    println!(
+                        "Was not able to take the encryption file path input -> {}",
+                        error
+                    );
+                }
+            };
+            return (file_path, encrypted_path);
+        }
+        Cryptography::DECRYPTION => {
+            println!("Enter the decrypted file path: ");
+            let _ = match io::stdin().read_line(&mut decrypted_path) {
+                Ok(_contents) => {}
+                Err(error) => {
+                    println!(
+                        "Was not able to take the decryption file path input -> {}",
+                        error
+                    );
+                }
+            };
+            return (file_path, decrypted_path);
+        }
+    }
 }
