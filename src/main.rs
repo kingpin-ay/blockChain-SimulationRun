fe4a9f1359b695fc382795842ab1d2bc7f6b68c0
kingpin-ay <ayushmondal001@gmail.com>
2024-05-18 15:01:53 +0530

    feat: added the private and public key generation logic
    Added the main private and public key generation logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use rand::rngs::OsRng;
+use rsa::pkcs1::{EncodeRsaPrivateKey, EncodeRsaPublicKey};
+use rsa::{RsaPrivateKey, RsaPublicKey};
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Generating the private and public key -> ");
+    let _key_value = match generate_rsa_key_pair("key/private_key.pem", "key/public_key.pem") {
+        Ok(value) => value,
+        Err(_error) => (),
+    };
+}
+
+fn generate_rsa_key_pair(
+    private_key_path: &str,
+    public_key_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Generate an RSA private key
+    let mut rng = OsRng;
+    let bits = 2048;
+    let private_key = RsaPrivateKey::new(&mut rng, bits)?;
+    let public_key = RsaPublicKey::from(&private_key);
+
+    // Convert private key to PEM format
+    let private_pem = private_key.to_pkcs1_pem(Default::default())?;
+    let public_pem = public_key.to_pkcs1_pem(Default::default())?;
+
+    // Write the private key to a file
+    let mut private_key_file = File::create(private_key_path)?;
+    private_key_file.write_all(private_pem.as_bytes())?;
+
+    // Write the public key to a file
+    let mut public_key_file = File::create(public_key_path)?;
+    public_key_file.write_all(public_pem.as_bytes())?;
+
+    Ok(())
 }
